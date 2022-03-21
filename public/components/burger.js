a6a8944cec3723550d57376e0fa9b59dc02cac33
AndreasJe <gud.er.gud@gmail.com>
2022-03-21 07:13:47 +0100

    Cleanup and considerations
    Notifications have been redone to be more full filling.
    Restructed project to fit with a landing page.
    Cleanup code for double spacing and more.
    Removing leftovers

--- public/components/burger.js
+++ public/components/burger.js
@@ -13,9 +13,6 @@ function start() {
 function burgerMenu() {
   console.log("Page has been loaded");
   document.querySelector("#menuknap").addEventListener("click", toggleMenu);
-  document
-    .querySelector("#reorderActivate")
-    .addEventListener("click", toggleReorder);
 }
 
 //Burger menu - toggle drawer
@@ -26,11 +23,11 @@ function toggleMenu() {
   let erSkjult = document.querySelector("#drawer").classList.contains("h-0");
 
   if (erSkjult == true) {
-    document.querySelector("#menuknap img").src = "icons/burger.svg";
+    document.querySelector("#menuknap img").src = "../icons/burger.svg";
     document.querySelector("#drawer").classList.add("invisible");
     document.querySelector("#drawer").classList.add("h-0");
   } else {
-    document.querySelector("#menuknap img").src = "icons/cross.svg";
+    document.querySelector("#menuknap img").src = "../icons/cross.svg";
     document.querySelector("#drawer").classList.remove("invisible");
     document.querySelector("#drawer").classList.add("h-fit");
   }
